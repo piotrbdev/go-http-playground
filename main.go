d60570fe294f76b92de772990db75aca36177076
Piotr Bargiel <piotrbdev@gmail.com>
2026-01-30 20:40:32 +0100

    add hello endpoint with name query param

--- main.go
+++ main.go
@@ -29,9 +29,28 @@ func health(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(Response{Status: "ok"})
 }
 
+func hello(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodGet {
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		return
+	}
+
+	name := r.URL.Query().Get("name")
+	fmt.Println(name)
+	if name == "" {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	message := "Hello " + name + "!"
+	json.NewEncoder(w).Encode(Response{Message: message})
+}
+
 func main() {
 	http.HandleFunc("/ping", ping)
 	http.HandleFunc("/health", health)
+	http.HandleFunc("/hello", hello)
 
 	fmt.Println("Server running on :8080")
 	http.ListenAndServe(":8080", nil)
